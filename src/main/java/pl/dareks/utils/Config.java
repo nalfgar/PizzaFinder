3dc5a48b1d7eb29db47667212f947d6d30038dfc
Darek Strojecki <dstroj@wp.pl>
2017-11-08 16:28:40 +0100

    Create HttpRequest.class

---
+++ src/main/java/pl/dareks/utils/Config.java
@@ -0,0 +1,9 @@
+package pl.dareks.utils;
+
+public class Config {
+    public static final String API_URL = "https://maps.googleapis.com/maps/api/place/textsearch/json?query=restaurants+in+Cracow+Grodzka&key=";
+    public static final String API_ID = "AIzaSyAZmYwNt_jfU8bvxVrogipeGvnsp9W99Cw";
+}
+
+
+//https://maps.googleapis.com/maps/api/place/textsearch/json?query=restaurants+in+Cracow+Grodzka&key=
